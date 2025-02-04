dbb2a2ecf91dd877327d4890edd7a62db95275f1
Rodrigo Camino <rodrigo.camino@vml.com>
2025-02-04 18:31:16 -0300

    Configure vercel.json agregue proxy 3

--- proxy.js
+++ proxy.js
@@ -1,8 +1,9 @@
 export default async (req, res) => {
+  if (req.method === "POST") {
     try {
       const response = await fetch("https://script.google.com/macros/s/AKfycbyzwNUuRPbCIQR7ftiVQR15Ul3T6GqAp0W67jqQRiZnVgQA_54kYY1--QpOVHZKhIRC/exec", {
         method: "POST",
-        body: req.body,
+        body: JSON.stringify(req.body),
         headers: {
           "Content-Type": "application/json",
         },
@@ -12,4 +13,7 @@ export default async (req, res) => {
     } catch (error) {
       res.status(500).json({ status: "error", message: error.message });
     }
-  };
+  } else {
+    res.status(405).json({ status: "error", message: "Method Not Allowed" });
+  }
+};
